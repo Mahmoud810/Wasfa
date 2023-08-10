a34f8bc303921a03d7c8a2e7c453766bb656dde8
Mahmoud810 <57998877+Mahmoud810@users.noreply.github.com>
2023-08-10 21:19:22 +0300

    first commit

---
+++ Wasfa/WasfaApp.swift
@@ -0,0 +1,20 @@
+//
+//  WasfaApp.swift
+//  Wasfa
+//
+//  Created by Mahmoud on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WasfaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
